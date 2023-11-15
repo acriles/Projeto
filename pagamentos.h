9e15d6e81866fb663e2fb152fe2c5c5e4feb4bec
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-11-15 11:26:30 -0300

    pagamentos.h

--- pagamentos.h
+++ pagamentos.h
@@ -8,8 +8,8 @@ Dar as opções de pagamento (imprimir na tela):
 * Detalhar cada opção
 PIX -> Imprimir na tela a chave do pix;
 Dinheiro -> Imprimir: "Pagamento na entrega/retirada".
-Cartão de Crédito: Solicitar número do cartão (16 dígitos); data de validade; CVV [12156165146414; 12, 20, 222]
-Cartão de Débito:  Solicitar número do cartão (16 dígitos); data de validade; CVV
+Cartão de Crédito: Solicitar nome do titular; número do cartão (16 dígitos); data de validade; CVV [12156165146414; 12, 20, 222]
+Cartão de Débito:  Solicitar nome do titular; número do cartão (16 dígitos); data de validade; CVV
 -------------------------------------------------------------------------------------------------------------------------------------------*/
 #include <string>
 
@@ -17,3 +17,10 @@ Cartão de Débito:  Solicitar número do cartão (16 dígitos); data de validad
 #define PAGAMENTOS_H
 
 //Struct Cartao: armazena os dados do cartão de crédito/debito a ser utilizado no pagamento
+struct Cartao {
+   std::string nomeTitular;
+   long long int numeroCartao; // Usando long long int para representar um número de 16 bits
+   int mesValidade;
+   int anoValidade;
+   int cvv;
+};
