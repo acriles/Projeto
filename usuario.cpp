1a3391fe7edc67033d9475c37e6706e7523581e4
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-11-17 17:57:23 -0300

    Update usuario.cpp

--- usuario.cpp
+++ usuario.cpp
@@ -53,12 +53,12 @@ using namespace std;
        cin >> telefone;
        cout << "Por ultimo, insira seu cpf" << endl;
        cin >> cpf;
-        cout << "Cadastro realizado com sucesso!Caso queira prosseguir com o pedido, digite 1, caso contrário digite 2";
+        cout << "Cadastro realizado com sucesso!Caso queira prosseguir com o pedido, digite 1, caso contrário digite 2" <<endl;
         cin >> confirmacaoPedido;
        if (confirmacaoPedido==1){
-         cout << "Prosseguindo com pedido";
+         cout << "Prosseguindo com pedido" <<endl;
        } else {
-         cout << "Ficamos aguardando por você!";
+         cout << "Ficamos aguardando por você!" <<endl;
        }
      }
 
@@ -66,3 +66,22 @@ using namespace std;
       cout << "Confirmando seus dados:" << endl 
       << "Nome:" <<  nome << endl << "Telefone:" << telefone << endl << "Cpf:" << cpf << endl;
      }
+void Usuario::PossuiCadastro() {
+  unsigned cadastro = 0;
+  cout << "Possui cadastro?" << endl
+       << "se sim digite 1, se nao digite 0" << endl;
+  cin >> cadastro;
+  if (cadastro == 1) {
+     Fazer_login();
+  } else
+    cadastroInicial();
+}
+void Usuario::Fazer_login() {
+  int cpf_l;
+  cout << "Insara seu cpf" << endl;
+  cin >> cpf_l;
+  if (cpf_l == cpf)
+    cout << "Login realizado com sucesso" << endl;
+  else
+    cout << "Cpf incorreto, tente novamente" << endl;
+}
