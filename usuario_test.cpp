954a61de269f99ea361aa0691c2b7673d72646ec
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-11-17 16:46:01 -0300

    Update usuario_test.cpp

--- usuario_test.cpp
+++ usuario_test.cpp
@@ -21,29 +21,6 @@ TEST_CASE("Usuario initial size is 0") {
   CHECK_EQ(user.getEndereco(), "Rua sara");
   // CHECK_EQ(user.getTelefone(), 318976);
 }
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
-#include "doctest.hpp"
-#include "usuario.hpp"
-
-/*TEST_CASE("Usuario initial size is 0") {
-  Usuario user;
-  user.setCpf(34);
-  CHECK_EQ(user.getCpf(), 34);
-}*/
-TEST_CASE("Usuario initial size is 0") {
-  Usuario user;
-  // user.cadastroInicial();
-  user.setNome("samuel");
-  user.setEndereco("Rua sara");
-  user.setCpf(123456789);
-  user.setTelefone(318976);
-
-  CHECK_EQ(user.getCpf(), 123456789);
-  CHECK_EQ(user.getNome(), "samuel");
-  CHECK_EQ(user.getEndereco(), "Rua sara");
-  // CHECK_EQ(user.getTelefone(), 318976);
-}
 
 TEST_CASE("TESTANDO USER") {
   Usuario user = Usuario("samuel", "sara", 123, 456);
