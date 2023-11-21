6c5240c8121812274d0ad49593c8d09136ddedd3
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-11-21 10:10:17 -0300

    Update pagamentos.cpp

--- pagamentos.cpp
+++ pagamentos.cpp
@@ -1,26 +1,3 @@
 
-	cin>>numero;
-	
-	while(verificaNumero(numero, cartao)==0){
-		cout<<"Número do cartão incorreto. Digite novamente: ";     //TESTCASE
-		cin>>numero;
-	}
-	
-	
-	//Coleta mês de validade
-	cout<<"Digite o mês de validade: ";
-	int mes;
-	cin>>mes;
-	
-	while (verificaMes(mes, cartao)==0){
-		cout<<"Mês inválido! Digite novamente: ";
-		cin>>mes;
-	}
-	
-	
-	//Coleta ano de validade
-
-	cout<<"Digite o ano validade (2 últimos dígitos): ";          
-
 	
 
