df257253dd79c85ec7b7fc59212f084284de7dd6
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-11-21 11:00:04 -0300

    pizza.h

--- pizza.h
+++ pizza.h
@@ -16,18 +16,18 @@ private:
 public:
     Pizza();     //Construtor 
     Pizza(string _sabor, string _tamanho, vector <string> _adicionais, float _valor);
-void setSabor(string _sabor);
-void setTamanho(float _valor);
-void setTamanho(string _tamanho);
-string getSabor();
-string getTamanho();
-float getValor();
-void addAdicional(string _adicional);
-void setAdicionais(vector <string> _adicionais); 
-void imprimePizza();
-void imprimeAdicionais();
-void inicializandoPizza();
-~Pizza(void); //destrutor 
+    void setSabor(string _sabor);
+    void setTamanho(float _valor);
+    void setTamanho(string _tamanho);
+    string getSabor();
+    string getTamanho();
+    float getValor();
+    void addAdicional(string _adicional);
+    void setAdicionais(vector <string> _adicionais); 
+    void imprimePizza();
+    void imprimeAdicionais();
+    void inicializandoPizza();
+    ~Pizza(void); //destrutor 
    
 };
 
