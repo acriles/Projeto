ef42135b071307bcb0273d5503e3f74676110f4c
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-11-21 10:50:02 -0300

    Update bebida.h

--- bebida.h
+++ bebida.h
@@ -4,26 +4,30 @@ using namespace std;
 
 #ifndef BEBIDA_H
 #define BEBIDA_H
+
+
 class Bebida {
 private:
-  string sabor;
-  string tipo;
-  string tamanho;
-  float valor;
+    string sabor;
+    string tipo;
+    string tamanho;
+    float valor;
 
 public:
-  Bebida();
-  Bebida(string _tipo, string _sabor, string _tamanho, float _valor);
-  void setTipo(string _tipo);
-  void setSabor(string _sabor);
-  void setTamanho(string _tamanho);
-  void setValor(float _valor);
-  string getSabor();
-  string getTipo();
-  string getTamanho();
-  float getValor();
-  void imprimeBebida();
-  void inicializandoBebida();
-  ~Bebida(void); // destrutor
+Bebida();
+Bebida(string _tipo, string _sabor, string _tamanho, float _valor);
+void setTipo(string _tipo);
+void setSabor(string _sabor);
+void setTamanho(string _tamanho);
+void setValor(float _valor);
+string getSabor();
+string getTipo();
+string getTamanho();
+float getValor();
+void imprimeBebida();
+void inicializandoBebida();
+ ~Bebida(void); // destrutor
 };
+
+
 #endif 
