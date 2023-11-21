e91bd0b2fe56bf75ffbcbf8b1c3f8d9c664c3b7d
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-11-21 10:43:48 -0300

    pagamentos.h

--- pagamentos.h
+++ pagamentos.h
@@ -7,20 +7,20 @@
 //Struct Cartao: armazena os dados do cartão de crédito/débito a ser utilizado no pagamento
 struct Cartao {
 public:
-   string nomeTitular;
-   long long int numeroCartao; // Usando long long int para representar um número de 16 bits
-   int mesValidade;
-   int anoValidade;
-   int cvv;
+    string nomeTitular;
+    long long int numeroCartao; // Usando long long int para representar um número de 16 bits
+    int mesValidade;
+    int anoValidade;
+    int cvv;
 };
 
 
 class Pagamentos{	
 public:
-	Cartao cartaoCred;
-        Cartao cartaoDeb;
-	void PIX();
-	void Dinheiro();
+    Cartao cartaoCred;
+    Cartao cartaoDeb;
+    void PIX();
+    void Dinheiro();
 };
 
 #endif
