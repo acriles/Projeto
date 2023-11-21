6591404802a573aa518289aa6a1d871a4d910f36
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-11-21 11:15:20 -0300

    usuario_test.cpp

--- usuario_test.cpp
+++ usuario_test.cpp
@@ -3,23 +3,24 @@
 #include "doctest.hpp"
 #include "usuario.hpp"
 
-TEST_CASE("Criando usario com set") {
-  Usuario user({},{},{},{});
-  user.setNome("samuel");
-  user.setEndereco("Rua sara");
-  user.setCpf(123456789);
-  user.setTelefone(318976);
 
-  CHECK_EQ(user.getCpf(), 123456789);
-  CHECK_EQ(user.getNome(), "samuel");
-  CHECK_EQ(user.getEndereco(), "Rua sara");
-  // CHECK_EQ(user.getTelefone(), 318976);
+TEST_CASE("Criando usario com set"){
+    Usuario user({},{},{},{});
+    user.setNome("samuel");
+    user.setEndereco("Rua sara");
+    user.setCpf(123456789);
+    user.setTelefone(318976);
+
+    CHECK_EQ(user.getCpf(), 123456789);
+    CHECK_EQ(user.getNome(), "samuel");
+    CHECK_EQ(user.getEndereco(), "Rua sara");
+     // CHECK_EQ(user.getTelefone(), 318976);
 }
 
-TEST_CASE("Criando usario com o construtor") {
-  Usuario user = Usuario("samuel", "sara", 123, 456);
-  CHECK_EQ(user.getCpf(), 456);
-  CHECK_EQ(user.getNome(), "samuel");
-  CHECK_EQ(user.getEndereco(), "sara");
-  CHECK_EQ(user.getTelefone(), 123);
+TEST_CASE("Criando usario com o construtor"){
+    Usuario user = Usuario("samuel", "sara", 123, 456);
+    CHECK_EQ(user.getCpf(), 456);
+    CHECK_EQ(user.getNome(), "samuel");
+    CHECK_EQ(user.getEndereco(), "sara");
+    CHECK_EQ(user.getTelefone(), 123);
 }
