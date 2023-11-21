1786d94cc4c8ca1990eff5012ffe5c39c245dea2
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-11-21 10:16:59 -0300

    Update pagamentos.cpp

--- pagamentos.cpp
+++ pagamentos.cpp
@@ -1,6 +1,27 @@
 #include "pagamentos.hpp"
 #include <iostream>
 
+//*************************************************************TESTE CASES********************************************************************
+
+//Função que verifica se o número tem 16 dígitos:
+int verificaNumero(long long int num, Cartao& cartao){
+    int cont=0;
+    long long int aux=num;
+	
+    while(aux!=0){
+            aux/=10;
+	    cont++;
+    }
+	
+    if(cont==16){     //Se tiver 16 dígitos, o número do cartão é valido. Logo, podemos armazenar na struct
+		cartao.numeroCartao=num;
+		return 1;
+	}
+    else{
+		return 0;
+	}
+}
+
 
 	
 
