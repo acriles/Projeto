f301741234c7f52bd834177e9a2094abe2026b25
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-11-22 15:10:11 -0300

    Update pizza.cpp

--- pizza.cpp
+++ pizza.cpp
@@ -39,31 +39,48 @@ void Pizza::setSabor(string _sabor){
  }
 
  void Pizza::imprimeAdicionais(){
+   if (!adicionais.empty()) {
     cout << "Adicionais";
     for(string adc: adicionais){
         cout << adc << ", ";
     }
     cout << endl;
+   }
  }
 
  void Pizza::imprimePizza(){
-    cout << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl;
+    cout << endl <<"Pizza selecionada" << endl << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl;
     imprimeAdicionais();
  }
 
  void Pizza::inicializandoPizza(){
-   int confirma; string adicional;
+   int confirma= 1;
+  string adicional;
    cout << "Digite o sabor" << endl;
    cin >> sabor; 
    cout << "Digite o tamanho" << endl;
    cin >> tamanho;
-   // cout << "Deseja adicionar acrescimos? Caso sim, digite 1, caso contrario digite 2" << endl;
-   // cin >> confirma;
-   // if (confirma==1){
-   //    cout << "Digite o acrescimo que deseja:" << endl;
-   //    cin >> adicional;
-   //    addAdicional(adicional);
-   // }
+    int aux = 0;
+  while (confirma == 1) {
+    if (aux == 0) {
+      cout << "Deseja adicionar acrescimos? Caso sim, digite 1, caso contrario "
+              "digite 0"
+           << endl;
+    } else {
+      cout << "Deseja adicionar mais um acrescimo? caso sim, digite 1, caso "
+              "contrario digite 0"
+           << endl;
+    }
+    cin >> confirma;
+    if (confirma == 0)
+      break;
+    else {
+      cout << "Digite o acrescimo que deseja:" << endl;
+      cin >> adicional;
+      addAdicional(adicional);
+    }
+    aux = 1;
+  }
  }
 
 
@@ -71,6 +88,6 @@ void Pizza::setSabor(string _sabor){
     sabor.clear();
     tamanho.clear();
     adicionais.clear();
-    cout <<"Classe destruida" << endl;
+    cout <<"Classe Pizza destruida" << endl;
  }
 
