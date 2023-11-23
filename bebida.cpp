7c7c5c608eddbfd10925e976b3ea61facdba848b
Ana Clara <anaclara-faria@outlook.com>
2023-11-23 11:20:44 -0300

    comentando cpp

--- bebida.cpp
+++ bebida.cpp
@@ -1,10 +1,9 @@
 #include "bebida.h"
 #include <iostream>
 
-
+//Construtores classe Bebida 
 Bebida::Bebida(){
 }
-
 Bebida::Bebida(string _tipo, string _sabor, string _tamanho, float _valor){
     tipo = _tipo;
     sabor = _sabor;
@@ -12,47 +11,41 @@ Bebida::Bebida(string _tipo, string _sabor, string _tamanho, float _valor){
     valor = _valor;
 }
 
-
+//Metodos set e get 
 void Bebida::setSabor(string _sabor){
     sabor = _sabor; 
 }
-
 
 void Bebida::setTipo(string _tipo){
     tipo = _tipo; 
 }
 
-
 void Bebida::setTamanho(string _tamanho){ 
     tamanho = _tamanho; 
 }
-
 
 void Bebida::setValor(float _valor){ 
     valor = _valor;
 }
 
-
 string Bebida::getSabor(){
     return sabor; 
 }
-
 
 string Bebida::getTamanho(){
     return tamanho; 
 }
 
-
 float Bebida::getValor(){ 
     return valor; 
 }
 
-
+//Imrprime atributos de Bebuda
 void Bebida::imprimeBebida(){
     cout << "Tipo:" << tipo << endl << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl << "Valor:" << valor << endl;
 }
 
-
+//Inicializa os atributos de bebida 
 void Bebida::inicializandoBebida(){
     cout << "Qual o tipo de bebida você deseja?" << endl;
     cin >> tipo;
@@ -62,7 +55,7 @@ void Bebida::inicializandoBebida(){
     cin >> tamanho;
 }
 
-
+//Destrutor da classe
 Bebida::~Bebida(void){
     sabor.clear();
     tipo.clear();
