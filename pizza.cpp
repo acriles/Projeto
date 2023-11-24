c84b5bb1a523a60c43cca33ef08969b0ab8dc90f
LuisTieres <109988907+LuisTieres@users.noreply.github.com>
2023-11-24 11:24:06 -0300

    Update pizza.cpp

--- pizza.cpp
+++ pizza.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-//Construtores da classe Pizza 
  Pizza::Pizza(){  
  }
 
@@ -14,7 +13,6 @@ using namespace std;
    valor = _valor + (adicionais.size()*2);
  }
 
-//Metodos set e get dos atributos 
 void Pizza::setSabor(string _sabor){
     sabor = _sabor; 
  }
@@ -40,59 +38,38 @@ void Pizza::setSabor(string _sabor){
     valor = valor + 2;
  }
 
-//Imprime os adicionais da pizza
  void Pizza::imprimeAdicionais(){
-   if (!adicionais.empty()) {
-    cout << "Adicionais";
+    cout << "Adicionais: ";
+    
     for(string adc: adicionais){
         cout << adc << ", ";
     }
     cout << endl;
-   }
  }
 
-//Imprime os atributos colocados na pizza 
  void Pizza::imprimePizza(){
-    cout << endl <<"Pizza selecionada" << endl << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl;
+    cout << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl;
     imprimeAdicionais();
  }
 
-//Inicializa os atributos da pizza
  void Pizza::inicializandoPizza(){
-   int confirma= 1;
-  string adicional;
+   int confirma; string adicional;
    cout << "Digite o sabor" << endl;
    cin >> sabor; 
    cout << "Digite o tamanho" << endl;
    cin >> tamanho;
-    int aux = 0;
-  while (confirma == 1) {
-    if (aux == 0) {
-      cout << "Deseja adicionar acrescimos? Caso sim, digite 1, caso contrario "
-              "digite 0"
-           << endl;
-    } else {
-      cout << "Deseja adicionar mais um acrescimo? caso sim, digite 1, caso "
-              "contrario digite 0"
-           << endl;
-    }
-    cin >> confirma;
-    if (confirma == 0)
-      break;
-    else {
+   cout << "Deseja adicionar acrescimos? Caso sim, digite 1, caso contrario digite 2" << endl;
+   cin >> confirma;
+   if (confirma==1){
       cout << "Digite o acrescimo que deseja:" << endl;
       cin >> adicional;
       addAdicional(adicional);
-    }
-    aux = 1;
-  }
+   }
  }
 
-//Destrutor da pizza
+
  Pizza::~Pizza(void){
     sabor.clear();
     tamanho.clear();
     adicionais.clear();
-    cout <<"Classe Pizza destruida" << endl;
  }
-
