e3ede97f3f5190fa6d4b996a3509625e2b838ace
LuisTieres <109988907+LuisTieres@users.noreply.github.com>
2023-11-26 15:59:14 -0300

    Update pizza.cpp

--- pizza.cpp
+++ pizza.cpp
@@ -1,21 +1,18 @@
 #include <iostream> 
 #include "pizza.h"
-
+#include "cardapio.h"
 using namespace std;
 
  Pizza::Pizza(){  
  }
 
- Pizza::Pizza(string _sabor, int _tamanho, vector <string> _adicionais, float _valor){
+ Pizza::Pizza(string _codigo,string _sabor, string _tamanho, vector <string> _adicionais, float _valor){
    sabor = _sabor;
    tamanho = _tamanho;
    adicionais = _adicionais;
    valor = _valor + (adicionais.size()*2);
+   codigo = _codigo;
  }
-
-float Pizza::getValor(){
-   return valor;
-}
 
 void Pizza::setSabor(string _sabor){
     sabor = _sabor; 
@@ -25,11 +22,11 @@ void Pizza::setSabor(string _sabor){
     return sabor;
  }
 
- void Pizza::setTamanho(int _tamanho){
+ void Pizza::setTamanho(string _tamanho){
     tamanho = _tamanho;
  }
 
-int Pizza::getTamanho(){
+ string Pizza::getTamanho(){
     return tamanho;
  }
  
@@ -52,41 +49,40 @@ int Pizza::getTamanho(){
  }
 
  void Pizza::imprimePizza(){
-    cout << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl << "Valor:" << valor << endl;
-    imprimeAdicionais(); 
-    cout << endl;
+    cout << "Código:" << codigo << endl << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl;
+    imprimeAdicionais();
  }
 
- void Pizza::inicializandoPizza(){
-   int confirma; string adicional;
-   cout << "Digite o sabor" << endl;
-   cin >> sabor; 
-   cout << "Digite o tamanho. Para uma pizza pequena digite 1, media digite 2 e grande digite 3" << endl;
-   cin >> tamanho;
-   calculaPreco();
-   cout << "Deseja adicionar acrescimos? Caso sim, digite 1, caso contrario digite 2" << endl;
-   cin >> confirma;
-   if (confirma==1){
-      cout << "Digite o acrescimo que deseja:" << endl;
-      cin >> adicional;
-      addAdicional(adicional);
-   }
- }
+ void Pizza::inicializandoPizza() {
+    int confirma;
+    string adicional;
 
-void Pizza::calculaPreco(){
-   if(tamanho==1){
-      valor = 39;
-   } else if (tamanho==2){
-      valor = 49;
-   } else if (tamanho==3){
-      valor = 59;
-   } else {
-      cout << "Tamanho invalido. Reiniciando a escolha do tamanho..." << endl;
-   }
+    cout << "Digite o código da pizza:" << endl;
+    cin >> codigo;
+
+    Cardapio cardapio;
+    if (cardapio.confirmar_codigo(codigo)) {
+        Cardapio cardapio;
+        cardapio.pedidos(codigo, sabor, tamanho, adicionais, valor); 
+        cout << "Deseja adicionar acréscimos? Caso sim, digite 1, caso contrário, digite 2" << endl;
+        cin >> confirma;
+
+        if (confirma == 1) {
+            cout << "Digite o acréscimo que deseja:" << endl;
+            cin >> adicional;
+            addAdicional(adicional);
+        }
+    }
+    else{
+         cout << "Comando invalido! " << endl;
+         inicializandoPizza();
+    }
 }
+
 
 
  Pizza::~Pizza(void){
     sabor.clear();
+    tamanho.clear();
     adicionais.clear();
  }
