24376ab3b667031a0179db990460bee89ada6da8
juliasvieira <146783645+juliasvieira@users.noreply.github.com>
2023-11-27 22:04:57 -0300

    Update pagamentos.cpp

--- pagamentos.cpp
+++ pagamentos.cpp
@@ -185,3 +185,40 @@ void coletaDadosCartao(Cartao& cartao)
 
 	
 }
+
+
+
+//SELEÇÃO DAS FORMAS DE PAGAMENTO-------------------------------------------------------------------
+
+int opcaoPagamento=Pagamentos.imprimeFormasPagamento();
+
+
+while(!(opcaoPagamento>=1 && opcaoPagamento<=4)){     //Verifica se a escolha está coerente com as opções apresentadas
+            std::cout<<std::endl;
+            std::cout<< "OPCAO INVALIDA! Digite novamente: ";
+            std::cin>>opcaoPagamento;			
+    }
+
+
+    switch (opcaoPagamento){
+    		case 1:
+    		pagamentos.PIX();
+    		break;
+    		
+    		case 2:
+    		pagamentos.Dinheiro();
+    		break;
+    		
+    		case 3:
+    		pagamentos.coletaDadosCartao(pagamentos.cartaoCred);
+    		break;
+    		
+    		case 4:
+    		pagamentos.coletaDadosCartao(pagamentos.cartaoDeb);
+    		break;
+    }
+    	
+    	
+    std::cout<<std::endl;
+    std::cout<<"PEDIDO CONFIRMADO! Sua pizza esta sendo preparada e saira para entrega em breve :)";
+    std::cout<<std::endl;
