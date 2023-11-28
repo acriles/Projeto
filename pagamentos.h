36c266fb094ada66e14ddd537cb3fedac4b587e0
juliasvieira <146783645+juliasvieira@users.noreply.github.com>
2023-11-27 22:22:59 -0300

    Update pagamentos.h

--- pagamentos.h
+++ pagamentos.h
@@ -9,19 +9,31 @@ using namespace std;
 struct Cartao {
 public:
     string nomeTitular;
-    long long int numeroCartao;     //Usando long long int para representar um número de 16 bits
+    long long int numeroCartao;     //Usando long long int para representar um número de 16 dígitos
     int mesValidade;
     int anoValidade;
     int cvv;
 };
 
 
-class Pagamentos{	
+class Pagamentos{
+
 public:
     Cartao cartaoCred;
     Cartao cartaoDeb;
+
+
+///@brief Imprime a chave PIX caso essa seja a forma de pagamento
     void PIX();
+
+///@brief Imprime que o pagamento será feito na entrega caso a forma selecionada seja
+///dinheiro e verifica se será necessário troco
     void Dinheiro();
+
+///@brief Imprime as formas de pagamento para o usuário escolher qual deseja
+    void imprimeFormasPagamento();
+
+///@brief coletaDadosCartao (
 };
 
 #endif
