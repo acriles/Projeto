0ab39459325392c8580728cb8c931601ecb2b2b1
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-11-27 22:03:30 -0300

    Update cardapio.cpp

--- cardapio.cpp
+++ cardapio.cpp
@@ -1,48 +1,77 @@
 #include <iostream>
 #include "cardapio.h"
 
-void Cardapio::inicializarCardapio(){
-    //inicializando as pizzas do cardapio 
-    pizzas.push_back(Pizza("#CP","Calabresa", "Pequena", {}, 39.0)); // {} ficam vazios, pois inicialmente nao tem acrescimos na pizza
-    pizzas.push_back(Pizza("#CM","Calabresa", "Média", {}, 49.0));
-    pizzas.push_back(Pizza("#CG","Calabresa", "Grande", {}, 59.0));
-    pizzas.push_back(Pizza("#MP","Marguerita", "Pequena", {}, 39.0));
-    pizzas.push_back(Pizza("#MM","Marguerita", "Média", {}, 49.0));
-    pizzas.push_back(Pizza("#MG","Marguerita", "Grande", {}, 59.0));
-    pizzas.push_back(Pizza("#BP","Bacon", "Pequena", {}, 39.0));
-    pizzas.push_back(Pizza("#BM","Bacon", "Média", {}, 49.0));
-    pizzas.push_back(Pizza("#BG","Bacon", "Grande", {}, 59.0));
-    
-    //inicializando as bebidas do cardapio 
-    bebidas.push_back(Bebida("#RCL","Refrigerante", "Coca", "Lata", 5.00)); 
-    bebidas.push_back(Bebida("#RC6","Refrigerante", "Coca", "600ml", 7.00)); 
-    bebidas.push_back(Bebida("#RGL","Refrigerante", "Guarana", "Lata", 5.00));  
-    bebidas.push_back(Bebida("#RG6","Refrigerante", "Guarana", "600ml", 7.00)); 
-    bebidas.push_back(Bebida("#SL6","Suco", "Laranja", "600ml", 7.00)); 
+void Cardapio::inicializarCardapioPizza() {
+  // inicializando as pizzas do cardapio
+  pizzas.push_back(Pizza(
+      "#CP", "Calabresa", "Pequena", {},
+      39.0)); // {} ficam vazios, pois inicialmente nao tem acrescimos na pizza
+  pizzas.push_back(Pizza("#CM", "Calabresa", "Média", {}, 49.0));
+  pizzas.push_back(Pizza("#CG", "Calabresa", "Grande", {}, 59.0));
+  pizzas.push_back(Pizza("#MP", "Marguerita", "Pequena", {}, 39.0));
+  pizzas.push_back(Pizza("#MM", "Marguerita", "Média", {}, 49.0));
+  pizzas.push_back(Pizza("#MG", "Marguerita", "Grande", {}, 59.0));
+  pizzas.push_back(Pizza("#BP", "Bacon", "Pequena", {}, 39.0));
+  pizzas.push_back(Pizza("#BM", "Bacon", "Média", {}, 49.0));
+  pizzas.push_back(Pizza("#BG", "Bacon", "Grande", {}, 59.0));
+}
+// inicializando as bebidas do cardapio
+void Cardapio::inicializarCardapioBebida() {
+  bebidas.push_back(Bebida("#RCL", "Refrigerante", "Coca", "Lata", 5.00));
+  bebidas.push_back(Bebida("#RC6", "Refrigerante", "Coca", "600ml", 7.00));
+  bebidas.push_back(Bebida("#RGL", "Refrigerante", "Guarana", "Lata", 5.00));
+  bebidas.push_back(Bebida("#RG6", "Refrigerante", "Guarana", "600ml", 7.00));
+  bebidas.push_back(Bebida("#SL6", "Suco", "Laranja", "600ml", 7.00));
 }
 
-bool Cardapio::confirmar_codigo(string codigo) {
-    inicializarCardapio();
-    for (int i = 0; i < pizzas.size(); i++) {
-        if (pizzas[i].getCodigo() == codigo) {
-            return true;
-        }
+bool Cardapio::confirmar_codigo_pizza(string codigo) {
+  inicializarCardapioPizza();
+  for (int i = 0; i < pizzas.size(); i++) {
+    if (pizzas[i].getCodigo() == codigo) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
-void Cardapio::pedidos(string codigo, string& _sabor, string& _tamanho, vector<string>& _adicionais, float& _valor) {
-    inicializarCardapio();
-    for (int i = 0; i < pizzas.size(); i++) {
-        if (pizzas[i].getCodigo() == codigo) {
-            _sabor = pizzas[i].getSabor();
-            cout << _sabor << endl;
-
-            _tamanho = pizzas[i].getTamanho();
-            //_valor = pizzas[i].getValor();
-            _adicionais = pizzas[i].getAdicionais();
-            break;
-        }
+bool Cardapio::confirmar_codigo_bebida(string codigo) {
+  inicializarCardapioBebida();
+  for (int i = 0; i < bebidas.size(); i++) {
+    if (bebidas[i].getCodigo() == codigo) {
+      return true;
     }
+  }
+  return false;
+}
+
+void Cardapio::pedidosPizza(string codigo, string &_sabor, string &_tamanho,
+                            vector<string> &_adicionais, float &_valor) {
+  inicializarCardapioPizza();
+  for (int i = 0; i < pizzas.size(); i++) {
+    if (pizzas[i].getCodigo() == codigo) {
+      _sabor = pizzas[i].getSabor();
+      cout << _sabor << endl;
+
+      _tamanho = pizzas[i].getTamanho();
+      //_valor = pizzas[i].getValor();
+      _adicionais = pizzas[i].getAdicionais();
+      break;
+    }
+  }
+}
+
+void Cardapio::pedidosBebida(string codigo, string &_sabor, string &_tamanho,
+                             float &_valor) {
+  inicializarCardapioBebida();
+  for (int i = 0; i < bebidas.size(); i++) {
+    if (bebidas[i].getCodigo() == codigo) {
+      _sabor = bebidas[i].getSabor();
+      cout << _sabor << endl;
+
+      _tamanho = pizzas[i].getTamanho();
+      //_valor = pizzas[i].getValor();
+      break;
+    }
+  }
 }
 
 void Cardapio::getPizzas(){
