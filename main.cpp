be6e6bceaa5851faf16fc2386b314c87b5e329bc
juliasvieira <146783645+juliasvieira@users.noreply.github.com>
2023-11-28 01:13:26 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,44 +18,6 @@ int main(){
     pedido.vetorPizzas();
     pedido.imprimePedido();
 
-    
-// //PAGAMENTO------------------------------------------
-//     Pagamentos pagamentos;
-    	
-//     int opcaoPagamento = pagamentos.imprimeFormasPagamento();
-    	
-//     while(!(opcaoPagamento>=1 && opcaoPagamento<=4)){     //Verifica se a escolha está coerente com as opções apresentadas
-//             std::cout<<std::endl;
-//             std::cout<< "OPCAO INVALIDA! Digite novamente: ";
-//             std::cin>>opcaoPagamento;			
-//     }
-    	
-//     std::cout<<std::endl;
-    
-    	
-//     switch (opcaoPagamento){
-//     		case 1:
-//     		pagamentos.PIX();
-//     		break;
-    		
-//     		case 2:
-//     		pagamentos.Dinheiro();
-//     		break;
-    		
-//     		case 3:
-//     		pagamentos.coletaDadosCartao(pagamentos.cartaoCred);
-//     		break;
-    		
-//     		case 4:
-//     		pagamentos.coletaDadosCartao(pagamentos.cartaoDeb);
-//     		break;
-//     }
-    	
-    	
-//     std::cout<<std::endl;
-//     std::cout<<"PEDIDO CONFIRMADO! Sua pizza esta sendo preparada e saira para entrega em breve :)";
-//     std::cout<<std::endl;
 
-    
     return 0;   
  }
