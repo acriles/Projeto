086c2448a7da30bfb5adce9d3eb1bd8881c7e446
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-03 20:01:48 -0300

    Create MenuPedido.h

---
+++ MenuPedido.h
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include "pedido.h"
+#include "Cardapio.h"
+using namespace std;
+
+class MenuPedido{
+private:
+Pedido pedido;
+Cardapio cardapio;
+
+public:
+MenuPedido(Pedido _pedido, Cardapio _cardapio);
+void Inicializar_MenuPedido();
+void Inicializar_Bebida();
+void Imprimir_Bebida();
+void Imprimir_Cardapio();
+};
