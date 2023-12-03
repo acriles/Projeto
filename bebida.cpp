d2e4150fab69404e9628c2b8ffbe5d3005d59931
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-03 19:54:55 -0300

    Update bebida.cpp

--- bebida.cpp
+++ bebida.cpp
@@ -1,67 +1,37 @@
 #include "bebida.h"
 #include <iostream>
-#include "cardapio.h"
 
-//Construtores classe Bebida 
-Bebida::Bebida(){
-}
-Bebida::Bebida(string _codigo ,string _tipo, string _sabor, string _tamanho, float _valor){
-    tipo = _tipo;
-    sabor = _sabor;
-    tamanho = _tamanho;
-    valor = _valor;
-    codigo = _codigo;
-}
-
-//Metodos set e get 
-void Bebida::setSabor(string _sabor){
-    sabor = _sabor; 
+// Construtores classe Bebida
+Bebida::Bebida() {}
+Bebida::Bebida(string _codigo, string _tipo, string _sabor, string _tamanho,
+               float _valor) {
+  tipo = _tipo;
+  sabor = _sabor;
+  tamanho = _tamanho;
+  valor = _valor;
+  codigo = _codigo;
 }
 
-void Bebida::setTipo(string _tipo){
-    tipo = _tipo; 
-}
+// Metodos set e get
+void Bebida::setSabor(string _sabor) { sabor = _sabor; }
 
-void Bebida::setTamanho(string _tamanho){ 
-    tamanho = _tamanho; 
-}
+void Bebida::setTipo(string _tipo) { tipo = _tipo; }
 
-void Bebida::setValor(float _valor){ 
-    valor = _valor;
-}
+void Bebida::setTamanho(string _tamanho) { tamanho = _tamanho; }
 
-string Bebida::getSabor(){
-    return sabor; 
-}
+void Bebida::setValor(float _valor) { valor = _valor; }
 
-string Bebida::getTamanho(){
-    return tamanho; 
-}
+string Bebida::getSabor() { return sabor; }
 
-float Bebida::getValor(){ 
-    return valor; 
-}
+string Bebida::getTamanho() { return tamanho; }
 
-//Imrprime atributos de Bebuda
-void Bebida::imprimeBebida(){
-    cout << "Tipo:" << tipo << endl << "Sabor:" << sabor << endl << "Tamanho:" << tamanho << endl << "Valor:" << valor << endl;
-}
+float Bebida::getValor() { return valor; }
 
-//Inicializa os atributos de bebida 
-void Bebida::inicializandoBebida(){
-  cout << "Digite o código da Bebida:" << endl;
-  cin >> codigo;
-  Cardapio cardapio;
-  if (cardapio.confirmar_codigo_bebida(codigo)) {
-    cardapio.pedidosBebida(codigo, sabor, tamanho, valor);
-  } else {
-    cout << "Comando invalido! " << endl;
-  }
-}
+string Bebida::getTipo() { return tipo; }
 
-//Destrutor da classe
-Bebida::~Bebida(void){
-    sabor.clear();
-    tipo.clear();
-    tamanho.clear();
+// Destrutor da classe
+Bebida::~Bebida(void) {
+  sabor.clear();
+  tipo.clear();
+  tamanho.clear();
 }
