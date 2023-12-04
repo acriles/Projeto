f5221298b936a1292981e67ba6f046e4ec6b9c62
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-12-04 10:13:33 -0300

    Update cardapio.h

--- cardapio.h
+++ cardapio.h
@@ -2,16 +2,23 @@
 #include <vector>
 #include "bebida.h"
 #include "pizza.h"
-
 using namespace std;
 
 class Cardapio{
-  private: 
+private: 
     vector <Bebida> bebidas; 
     vector <Pizza> pizzas;
 
   public:
+
+///@brief Construtor padrão da classe Cardapio
     Cardapio();
+///@brief Obtém o vetor de bebidas do cardápio
+///@return Vetor contendo as bebidas disponíveis no cardápio
     vector<Bebida> getBebidas();
+
+///@brief Obtém o vetor de pizzas do cardápio
+///@return Vetor contendo as pizzas disponíveis no cardápio
     vector<Pizza> getPizzas();
+
 };
