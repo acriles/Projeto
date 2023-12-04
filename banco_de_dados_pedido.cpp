5edf8e4dca30f8568fc4a378cd97f1045d3bce59
LuisTieres <109988907+LuisTieres@users.noreply.github.com>
2023-12-04 19:44:04 -0300

    Create banco_de_dados_pedido.cpp

---
+++ banco_de_dados_pedido.cpp
@@ -0,0 +1,77 @@
+#include "banco_de_dados_pedido.h"
+
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/statement.h>
+#include <cppconn/resultset.h>
+
+void Data_base_Pedido:: inserir_pedidos(int cpf, string infoPizza, string infoBebida) {
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+
+    string connectionString = "tcp://roundhouse.proxy.rlwy.net:38644";
+    string username = "root";
+    string password = "acA5dg3CAF4CBe25-FGc3Hgffga5C3Ge";
+    string database = "railway";
+
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect(connectionString, username, password);
+
+    con->setSchema(database);
+
+    sql::Statement *stmt = con->createStatement();
+    cout<<infoBebida<<endl;
+    string query = "INSERT INTO pedido (cp, infoPizza,infoBebida) VALUES (" + to_string(cpf) + ", '" + infoPizza + "', '" + infoBebida + "')";
+
+
+    stmt->execute(query);
+
+
+    delete stmt;
+    delete con;
+}
+
+
+
+
+void Data_base_Pedido::ler_pedido(int& _cpf, int tipo) {
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+
+    string connectionString = "tcp://roundhouse.proxy.rlwy.net:38644";
+    string username = "root";
+    string password = "acA5dg3CAF4CBe25-FGc3Hgffga5C3Ge";
+    string database = "railway";
+
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect(connectionString, username, password);
+
+    con->setSchema(database);
+
+    sql::Statement *stmt = con->createStatement();
+    sql::ResultSet *res;
+
+    string query = "SELECT * FROM pedido";
+    cout << 4<<endl;
+    res = stmt->executeQuery(query);
+
+    while (res->next()) {
+        int cpf_analise = res->getInt("cp");
+        if (_cpf == cpf_analise && tipo == 1){
+            string pizza = res->getString("infoPizza");
+            
+            cout << pizza<<endl;
+        }
+        if (_cpf == cpf_analise && tipo == 2){
+            string bebida = res->getString("infoBebida");
+            
+
+            cout << bebida<< endl;
+        }
+    }
+
+    delete res;
+    delete stmt;
+    delete con;
+
+}
