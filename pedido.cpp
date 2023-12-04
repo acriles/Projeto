18c740a2bcb0963054c797c8d049930168d6e189
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-04 01:15:15 -0300

    Update pedido.cpp

--- pedido.cpp
+++ pedido.cpp
@@ -1,20 +1,32 @@
 #include "pedido.h"
 #include <iostream>
 Pedido::Pedido() {}
+
 void Pedido::InserirBebida(Bebida _bebida) { bebidaPedido.push_back(_bebida); }
+void Pedido::InserirPizza(Pizza _pizza) { pizzaPedido.push_back(_pizza); }
 
 vector<Bebida> Pedido::getBebidas() { return bebidaPedido; }
+vector<Pizza> Pedido::getPizzas() { return pizzaPedido; }
 
-bool Pedido::ExisteBebida(string _codigo) {
+
+bool Pedido::ExisteCodigo(string _codigo) {
   for (int i = 0; i < bebidaPedido.size(); i++) {
     if (_codigo == bebidaPedido[i].getCodigo())
       return true;
   }
+  for (int i = 0; i < pizzaPedido.size(); i++) {
+    if (_codigo == pizzaPedido[i].getCodigo())
+      return true;
+  }
   return false;
 }
-void Pedido::RemoverBebida(string _codigo) {
+void Pedido::RemoverPedido(string _codigo) {
   for (int i = 0; i < bebidaPedido.size(); i++) {
     if (_codigo == bebidaPedido[i].getCodigo())
       bebidaPedido.erase(bebidaPedido.begin() + i);
+  }
+  for (int i = 0; i < pizzaPedido.size(); i++) {
+    if (_codigo == pizzaPedido[i].getCodigo())
+      pizzaPedido.erase(pizzaPedido.begin() + i);
   }
 }
