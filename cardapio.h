a095c2e5c5a15785418ad38dc926b42cc08aa315
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-04 01:17:21 -0300

    Update cardapio.h

--- cardapio.h
+++ cardapio.h
@@ -1,15 +1,17 @@
 #include <string> 
 #include <vector>
-#include <iostream>
 #include "bebida.h"
+#include "pizza.h"
 
 using namespace std;
 
 class Cardapio{
   private: 
-    vector <Bebida> bebidas;  
+    vector <Bebida> bebidas; 
+    vector <Pizza> pizzas;
 
   public:
     Cardapio();
     vector<Bebida> getBebidas();
+    vector<Pizza> getPizzas();
 };
