1610670ad3e214c733d7c0aab8a62d6a0bc7fc10
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-12-04 10:28:53 -0300

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -5,16 +5,17 @@
 #include "pedido.h"
 #include "banco__de_dados.h"
 
-void Interface::PossuiCadastro() {
+void Interface::PossuiCadastro(){
     unsigned cadastro = 0;
     cout << "Seja Bem vindo! Você possui cadastro?" << endl
     << "Se sim digite 1, se nao digite qualquer tecla." << endl;
     cin >> cadastro;
     
-    if (cadastro == 1) {
+    if(cadastro == 1){
         Fazer_login();
         return;
-    } else{
+     } 
+    else{
         Usuario user({}, {}, {}, {});
 
         cout << "Iniciando o cadastro:" << endl;
@@ -22,7 +23,7 @@ void Interface::PossuiCadastro() {
     }
 }
 
-void Interface::Fazer_login() {
+void Interface::Fazer_login(){
     string name;
     int cpf_l;
     int confirmacaoPedido;
@@ -32,15 +33,15 @@ void Interface::Fazer_login() {
     
     Data_base data;
 
-    if (data.ler_banco_de_dados(cpf_l, name)) {
+    if(data.ler_banco_de_dados(cpf_l, name)){
         cout << "\nParabéns " + name +" o seu login foi realizado com sucesso! Caso queira prosseguir com o pedido, digite 1, caso contrário digite qualquer outra tecla.\n";
         cin >> confirmacaoPedido;
 
-        if (confirmacaoPedido == 1) { 
+        if(confirmacaoPedido == 1){ 
           cout << "Prosseguindo com pedido" << endl;
           return;
         } 
-        else {
+        else{
            cout << "Ficamos aguardando por você!" << endl;
            return;
         }
