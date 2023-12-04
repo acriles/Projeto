1862f5a069c77f2ebd5ae8e62924958221629a36
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-12-04 08:58:31 -0300

    Update MenuPedido.h

--- MenuPedido.h
+++ MenuPedido.h
@@ -6,15 +6,21 @@ using namespace std;
 
 class MenuPedido{
 private:
-Pedido pedido;
-Cardapio cardapio;
+    Pedido pedido;
+    Cardapio cardapio;
 
 public:
-MenuPedido(Pedido _pedido, Cardapio _cardapio);
-void Inicializar_MenuPedido();
-void Inicializar_Pedido();
-void Imprimir_Pedido();
-void Imprimir_Cardapio();
-void Remover_Pedido();
-void Valor_Total();
+
+///@brief Construtor que inicializa um MenuPedido com um Pedido e um Cardápio
+///@param _pedido O pedido a ser associado ao MenuPedido
+///@param _cardapio O cardápio a ser associado ao MenuPedido
+    MenuPedido(Pedido _pedido, Cardapio _cardapio);
+
+    void Inicializar_MenuPedido();     //Inicializa o MenuPedido
+    void Inicializar_Pedido();     //Inicializa o Pedido
+    void Imprimir_Pedido();     //Imprime o Pedido atual
+    void Imprimir_Cardapio();     //Imprime o Cardápio disponível
+    void Remover_Pedido();     //Remove o Pedido atual
+    void Valor_Total();     //Calcula e exibe o valor total do Pedido
+
 };
