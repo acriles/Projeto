b32a048f993c0b70ec1743c8254ea100370cb462
juliasvieira <146783645+juliasvieira@users.noreply.github.com>
2023-12-04 11:47:44 -0300

    Update cartao.cpp

--- cartao.cpp
+++ cartao.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //TRATAMENTOS DE EXCEÇÃO----------------------------------------------------
 
 //Função que verifica se o número tem 16 dígitos ----------------------------------------------------
-int Cartao::VerificaNumero(string num){
+int Cartao::Verifica_Numero(string num){
     int cont=0;
    	
 	for (char c:num){
@@ -28,7 +28,7 @@ int Cartao::VerificaNumero(string num){
 
 
 //Função que verifica se o mês é válido -----------------------------------------------------
-int Cartao::VerificaMes(int m){
+int Cartao::Verifica_Mes(int m){
     if(m>=1 && m<=12){              //O mês deve estar entre 01 (janeiro) e 12 (fevereiro)
             mesValidade=m;
 	    return 1;
@@ -41,12 +41,11 @@ int Cartao::VerificaMes(int m){
 
 
 //Função que verifica se o ano é válido----------------------------------------------------
-int Cartao::VerificaAno(int a){
+int Cartao::Verifica_Ano(int a){
     if(a>=23 && a<=40){     //Supondo que um cartão atual não tenha validade superior ao ano de 2040
             anoValidade=a;
 	    return 1;
-    }
-	    
+    }	    
     else{
             return 0;
     }
@@ -54,7 +53,7 @@ int Cartao::VerificaAno(int a){
 
 
 //Função que verifica se o CVV é válido -----------------------------------------------------
-int Cartao::VerificaCvv(int cod)
+int Cartao::Verifica_Cvv(int cod)
 {
     int cont=0;
     long long int aux=cod;
@@ -62,13 +61,11 @@ int Cartao::VerificaCvv(int cod)
     while(aux!=0){
             aux/=10;
             cont++;
-    }
-	
+    }	
     if(cont==3){             //Se tiver 3 dígitos, o CVV digitado é valido. Logo, podemos armazenar na struct
             cvv=cod;
             return 1;
-    }
-	    
+    }	    
     else{
             return 0;
     }
@@ -82,56 +79,56 @@ int Cartao::VerificaCvv(int cod)
 Cartao::Cartao() {
 }
 
-void Cartao::ColetaDadosCartao ()
+void Cartao::Coleta_Dados_Cartao ()
 {
 //Coleta nome do titular
-    cout<<"Digite o nome do titular do cartao: ";
+    cout<<"Digite o nome do titular do cartão: ";
     cin>>nomeTitular;
 	
 //Coleta numero do cartão
     cout<<endl;
-    cout<<"Digite o numero do cartao (16 digitos): ";
+    cout<<"Digite o número do cartão (16 dígitos): ";
     string numero;
     getline(cin, numero);
 	
-    while(!(VerificaNumero(numero))){
-            cout<<"Numero do cartao incorreto. Digite novamente: ";     
+    while(!(Verifica_Numero(numero))){
+            cout<<"Número do cartão incorreto. Digite novamente: ";     
             getline(cin, numero);
 }
 
 	
 //Coleta mês de validade
     cout<<endl;
-    cout<<"Digite o mes de validade: ";
+    cout<<"Digite o mês de validade: ";
     int mes;
     cin>>mes;
 	
-    while(!(VerificaMes(mes))){
-            cout<<"Mes invalido! Digite novamente: ";
+    while(!(Verifica_Mes(mes))){
+            cout<<"Mês inválido! Digite novamente: ";
             cin>>mes;
     }
 
 	
 //Coleta ano de validade
 	cout<<endl;
-	cout<<"Digite o ano validade (2 ultimos digitos): ";          
+	cout<<"Digite o ano validade (2 últimos dígitos): ";          
 	int ano;
 	cin>>ano;
 	
-	while (!(VerificaAno(ano))){
-		cout<<"Ano invalido! Digite novamente: ";     
+	while(!(Verifica_Ano(ano))){
+		cout<<"Ano inválido! Digite novamente: ";     
 		cin>>ano;
 	}
 
 	
 //Coleta cvv
     cout<<endl;
-    cout<<"Digite o codigo de seguranca do cartao (CVV): ";
+    cout<<"Digite o código de segurança do cartão (CVV): ";
     int codigo;
     cin>>codigo;
 	
-    while(!(VerificaCvv(codigo))){
-            cout<<"CVV invalido! Digite novamente: ";     
+    while(!(Verifica_Cvv(codigo))){
+            cout<<"CVV inválido! Digite novamente: ";     
             cin>>codigo;
 	}
 	
