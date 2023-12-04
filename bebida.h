f7114ed01b5df366b0d02a3bdfecbc5bf940f7c4
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-12-04 09:33:17 -0300

    Update bebida.h

--- bebida.h
+++ bebida.h
@@ -5,29 +5,27 @@ using namespace std;
 #ifndef BEBIDA_H
 #define BEBIDA_H
 
-
-class Bebida {
+class Bebida{
 private:
-    string sabor;    //sabor da bebida
-    string tipo;     //tipo da bebida
-    string tamanho;  //tamanho da bebida
-    float valor;     //valor da bebida
-    string codigo;   //codigo de identificacao da bebida
+    string sabor;     //Sabor da bebida
+    string tipo;     //Tipo da bebida
+    string tamanho;  //Tamanho da bebida
+    float valor;     //Valor da bebida
+    string codigo;   //Codigo de identificacao da bebida
 
 public:
 ///@brief Inicializa os membros da classe
-///@param _codigo Codigo utilizado para identificar cada bebida
+///@param _codigo Código utilizado para identificar cada bebida
 ///@param _tipo Tipo de bebida - refrigerante ou suco
 ///@param _sabor Sabor da bebida
 ///@param _tamanho Tamanho da bebida
 ///@param _valor Valor da bebida escolhida
     Bebida(string _codigo ,string _tipo, string _sabor, string _tamanho, float _valor);
 
-///@brief Obtem o codigo de identificação da bebida
-///@return Codigo da bebida
-    string getCodigo() const 
-    {
-    return codigo;
+///@brief Obtém o código de identificação da bebida
+///@return Código da bebida
+    string getCodigo() const{
+        return codigo;
     }//nao apareceu o commit
 
 ///@brief Obtem o sabor da bebida escolhida
