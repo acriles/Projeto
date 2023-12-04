7ebff20ca969d0f1542b7048c6b8b2b003f9de6d
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-04 17:01:31 -0300

    Create excecao.hpp

---
+++ excecao.hpp
@@ -0,0 +1,18 @@
+#ifndef EXCECAO_HPP
+#define EXCECAO_HPP
+#include <stdexcept>
+#include <iostream>
+class CodigoInvalido : public std::exception{
+private:
+  std::string opcao;
+  std::string mensagem;
+public:
+CodigoInvalido(std::string t){
+  opcao = t;
+  mensagem = "A opção escolhida não existe.\nDigite uma Codigo valida:";
+}
+  const char* what() const throw() override{
+    return mensagem.c_str();
+  }
+};
+#endif
