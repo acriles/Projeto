a482a80d4e1a4262771a87c1138173b79b88954d
vluizlr <139981746+vluizlr@users.noreply.github.com>
2023-12-04 08:44:52 -0300

    pizza.h

--- pizza.h
+++ pizza.h
@@ -5,51 +5,50 @@ using namespace std;
 #ifndef PIZZA_H
 #define PIZZA_H
 
-
 class Pizza{
 private:
-     string sabor;                   //sabor da pizza
-     string tamanho;                //tamanho da pizza
-     float valor;                  //valor da pizza
-     string codigo;              // codigo de identificacao da pizza
+    string sabor;     //Sabor da pizza
+    string tamanho;     //Tamanho da pizza
+    float valor;     //Valor da pizza
+    string codigo;     //Código de identificação da pizza
 
 
 public:
 
 ///@brief Inicializa os membros da classe
-///@param _codigo Codigo utilizado para identificar cada pizza
+///@param _codigo Código utilizado para identificar cada pizza
 ///@param _sabor Sabor da pizza
 ///@param _tamanho Tamanho da pizza
 ///@param _adicionais Vetor que contempla os adicionais da pizza
 ///@param _ valor Valor da pizza
      Pizza(string _codigo,string _sabor, string _tamanho, float _valor);
 
-///@brief Obtem o codigo de identificacao da pizza
-///@return Codigo da pizza
-     string getCodigo() const{
+///@brief Obtém o código de identificação da pizza
+///@return Código da pizza
+    string getCodigo() const{
         return codigo;
     }
 
-///@brief Obtem o sabor da pizza escolhida
+///@brief Obtém o sabor da pizza escolhida
 ///@return Sabor da pizza
-    string getSabor() const {
+    string getSabor() const{
         return sabor;
     }
 
-///@brief Obtem o tamanho da pizza escolhida
+///@brief Obtém o tamanho da pizza escolhida
 ///@return Tamanho da pizza
-    string getTamanho() const {
+    string getTamanho() const{
         return tamanho;
     }
 
-///@brief Obtem o valor da pizza escolhida
+///@brief Obtém o valor da pizza escolhida
 ///@return Valor da pizza
-    float getValor() const {
-    return valor;
+    float getValor() const{
+        return valor;
     }
 
 ///@Destrutor da classe Pizza
-      ~Pizza(void); //destrutor 
+    ~Pizza(void);     //Destrutor 
 
 };
 #endif // PIZZA_H
