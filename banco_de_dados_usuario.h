81dc4e32a5ec8a1e525d0bebb40f479e0990bfb0
juliasvieira <146783645+juliasvieira@users.noreply.github.com>
2023-12-04 11:49:14 -0300

    Update banco_de_dados_usuario.h

--- banco_de_dados_usuario.h
+++ banco_de_dados_usuario.h
@@ -19,12 +19,12 @@ public:
 ///@param _telefone Número de telefone do usuário a ser inserido
 ///@param _cpf CPF do usuário a ser inserido
 ///@return true se a inserção for bem-sucedida, false caso contrário
-    bool inserir_usuario(string _nome, string _endereco, int _telefone, int _cpf);
+    bool Inserir_Usuario(string _nome, string _endereco, int _telefone, int _cpf);
 
  ///@brief Lê o banco de dados para obter informações de um usuário com base no CPF
 ///@param _cpf CPF do usuário a ser consultado no banco de dados
 ///@param name Variável de referência para armazenar o nome do usuário encontrado
 ///@return true se o usuário com o CPF especificado for encontrado, false caso contrário
-    bool ler_banco_de_dados(int& _cpf, string& name); 
+    bool Ler_Banco_De_Dados(int& _cpf, string& name); 
 
 };
