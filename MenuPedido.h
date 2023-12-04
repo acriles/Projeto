d39c7d3bb332257f5f4c12f50fa94831a46ce498
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-03 22:03:00 -0300

    Update MenuPedido.h

--- MenuPedido.h
+++ MenuPedido.h
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
-#include "pedido.h"
-#include "Cardapio.h"
+#include "pedido.hpp"
+#include "Cardapio.hpp"
 using namespace std;
 
 class MenuPedido{
@@ -13,6 +13,7 @@ public:
 MenuPedido(Pedido _pedido, Cardapio _cardapio);
 void Inicializar_MenuPedido();
 void Inicializar_Bebida();
-void Imprimir_Bebida();
+void Imprimir_Bebida_Pedido();
 void Imprimir_Cardapio();
+void Remover_Pedido();
 };
