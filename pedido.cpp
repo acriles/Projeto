6bbfe39295209a8a2cea7fbf176f6acb6ae8b4ac
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-03 22:04:00 -0300

    Update pedido.cpp

--- pedido.cpp
+++ pedido.cpp
@@ -4,3 +4,17 @@ Pedido::Pedido() {}
 void Pedido::InserirBebida(Bebida _bebida) { bebidaPedido.push_back(_bebida); }
 
 vector<Bebida> Pedido::getBebidas() { return bebidaPedido; }
+
+bool Pedido::ExisteBebida(string _codigo) {
+  for (int i = 0; i < bebidaPedido.size(); i++) {
+    if (_codigo == bebidaPedido[i].getCodigo())
+      return true;
+  }
+  return false;
+}
+void Pedido::RemoverBebida(string _codigo) {
+  for (int i = 0; i < bebidaPedido.size(); i++) {
+    if (_codigo == bebidaPedido[i].getCodigo())
+      bebidaPedido.erase(bebidaPedido.begin() + i);
+  }
+}
