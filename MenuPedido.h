a130ae3a7e6d4c58c104b296f1d230edca77378e
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-04 01:11:12 -0300

    Update MenuPedido.h

--- MenuPedido.h
+++ MenuPedido.h
@@ -12,8 +12,9 @@ Cardapio cardapio;
 public:
 MenuPedido(Pedido _pedido, Cardapio _cardapio);
 void Inicializar_MenuPedido();
-void Inicializar_Bebida();
-void Imprimir_Bebida_Pedido();
+void Inicializar_Pedido();
+void Imprimir_Pedido();
 void Imprimir_Cardapio();
 void Remover_Pedido();
+void Valor_Total();
 };
