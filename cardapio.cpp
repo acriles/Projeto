ab5dad5699785f6ba387baca499e4ada6f9bad4b
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-04 01:18:01 -0300

    Update cardapio.cpp

--- cardapio.cpp
+++ cardapio.cpp
@@ -1,14 +1,21 @@
 #include "Cardapio.h"
 #include <iostream>
 
-/*void Cardapio::inicializarCardapioBebida() {
-  bebidas.push_back(Bebida("#RCL", "Refrigerante", "Coca", "Lata", 5.00));
-  bebidas.push_back(Bebida("#RC6", "Refrigerante", "Coca", "600ml", 7.00));
-  bebidas.push_back(Bebida("#RGL", "Refrigerante", "Guarana", "Lata", 5.00));
-  bebidas.push_back(Bebida("#RG6", "Refrigerante", "Guarana", "600ml", 7.00));
-  bebidas.push_back(Bebida("#SL6", "Suco", "Laranja", "600ml", 7.00));
-}*/
+// CHAMA O CONSTRUTOR DE CARDAPIO COM OS VETORES JA PREENCHIDOS
 Cardapio::Cardapio()
     : bebidas{Bebida("#RCL", "Refrigerante", "Coca", "Lata", 5.00),
-              Bebida("#RC6", "Refrigerante", "pepsi", "600ml", 7.00)} {}
+              Bebida("#RC6", "Refrigerante", "pepsi", "600ml", 7.00),
+              Bebida("#RGL", "Refrigerante", "Guarana", "Lata", 5.00),
+              Bebida("#RG6", "Refrigerante", "Guarana", "600ml", 7.00),
+              Bebida("#SL6", "Suco", "Laranja", "600ml", 7.00)},
+      pizzas{Pizza("#CP", "Calabresa", "Pequena", 39.0),
+             Pizza("#CG", "Calabresa", "Grande", 59.0),
+             Pizza("#MP", "Marguerita", "Pequena", 39.0),
+             Pizza("#MM", "Marguerita", "Média", 49.0),
+             Pizza("#MG", "Marguerita", "Grande", 59.0),
+             Pizza("#BP", "Bacon", "Pequena", 39.0),
+             Pizza("#BM", "Bacon", "Média", 49.0),
+             Pizza("#BG", "Bacon", "Grande", 59.0)} {}
+
 vector<Bebida> Cardapio::getBebidas() { return bebidas; }
+vector<Pizza> Cardapio::getPizzas() { return pizzas; }
