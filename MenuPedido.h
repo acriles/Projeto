8bd786514b2ccf7ec753229f8da97f03dbf5ff29
LuisTieres <109988907+LuisTieres@users.noreply.github.com>
2023-12-04 19:45:39 -0300

    Update MenuPedido.h

--- MenuPedido.h
+++ MenuPedido.h
@@ -19,12 +19,12 @@ public:
 ///@param _cardapio O cardápio a ser associado ao MenuPedido
     MenuPedido(Pedido _pedido, Cardapio _cardapio);
 
-    void Inicializar_MenuPedido();     //Inicializa o MenuPedido
+    void Inicializar_MenuPedido(int& cpf);     //Inicializa o MenuPedido
     void Inicializar_Pedido();        //Inicializa o Pedido
-    void Imprimir_Pedido();          //Imprime o Pedido atual
+    void Imprimir_Pedido(int& cpf);          //Imprime o Pedido atual
     void Imprimir_Cardapio();       //Imprime o Cardápio disponível
     void Remover_Pedido();         //Remove o Pedido atual
-    void Valor_Total();           //Calcula e exibe o valor total do Pedido
+    void Valor_Total(int& cpf);           //Calcula e exibe o valor total do Pedido
 
 };
 #endif // MENUPEDIDO_H
