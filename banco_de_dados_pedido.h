028306d758e993f9e3b2d0b51986e0a0d8f354ff
Victor Luiz <139981746+vluizlr@users.noreply.github.com>
2023-12-04 23:38:33 -0300

    Update banco_de_dados_pedido.h

--- banco_de_dados_pedido.h
+++ banco_de_dados_pedido.h
@@ -1,7 +1,9 @@
 #include <string>
-using namespace std;
 #include <vector>
 
+using namespace std;
+
+///@brief Classe que representa um banco de dados para pedidos.
 class Data_base_Pedido{
     private:
      string infoPizza;
@@ -9,6 +11,14 @@ class Data_base_Pedido{
 
 
     public:
+///@brief Método para inserir novos pedidos no banco de dados.
+///@param _cpf CPF do cliente que está fazendo o pedido.
+///@param infoPizza Informações sobre a pizza que está sendo pedida.
+///@param infoBebida Informações sobre a bebida que está sendo pedida.
      void inserir_pedidos(int _cpf, string infoPizza, string infoBebida);
+
+///@brief Método para ler informações sobre pedidos existentes no banco de dados.
+///@param _cpf Referência para o CPF do cliente cujo pedido se deseja ler.
+///@param tipo Tipo de informação que se deseja ler sobre o pedido.
      void ler_pedido(int& _cpf, int tipo); 
 };
