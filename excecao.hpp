e29b2887d839d5f8380e011376891e89881268c9
samucavm2 <69234519+samucavm2@users.noreply.github.com>
2023-12-04 22:40:02 -0300

    Update excecao.hpp

--- excecao.hpp
+++ excecao.hpp
@@ -4,12 +4,25 @@
 #include <iostream>
 class CodigoInvalido : public std::exception{
 private:
-  std::string opcao;
+  std::string codigo;
   std::string mensagem;
 public:
 CodigoInvalido(std::string t){
+  codigo = t;
+  mensagem = "A opção escolhida não existe.\nDigite um Codigo valido, ou digite 0 para voltar ao menu inicial:\n";
+}
+  const char* what() const throw() override{
+    return mensagem.c_str();
+  }
+};
+class OpcaoInvalido : public std::exception{
+private:
+  string opcao;
+  std::string mensagem;
+public:
+OpcaoInvalido(string t){
   opcao = t;
-  mensagem = "A opção escolhida não existe.\nDigite uma Codigo valida:\n";
+  mensagem = "A opção escolhida não existe.\nDigite uma das opçoes abaixo:\n";
 }
   const char* what() const throw() override{
     return mensagem.c_str();
