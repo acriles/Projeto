ea295d189ed346f24213640a854be43cbd9f60bf
juliasvieira <146783645+juliasvieira@users.noreply.github.com>
2023-12-04 21:36:56 -0300

    Update pagamentos.h

--- pagamentos.h
+++ pagamentos.h
@@ -1,21 +1,11 @@
+#include "cartao.h"
 #include <string>
 #include <iostream>
-#include "cartao.h"
 
 using namespace std;
 
 #ifndef PAGAMENTOS_H
 #define PAGAMENTOS_H
-
-// //Struct Cartao: armazena os dados do cartão de crédito/débito a ser utilizado no pagamento
-// class Cartao {
-// public:
-//     string nomeTitular;
-//     long long int numeroCartao;     //Usando long long int para representar um número de 16 dígitos
-//     int mesValidade;
-//     int anoValidade;
-//     int cvv;
-// };
 
 class Pagamentos{
 
@@ -24,19 +14,13 @@ public:
 ///@brief Imprime a chave PIX caso essa seja a forma de pagamento
     void PIX();
 
-///@brief Imprime que o pagamento será feito na entrega caso a forma selecionada seja dinheiro e verifica se será necessário troco
+///@brief Imprime que o pagamento será feito na entrega caso a forma selecionada seja
+///    dinheiro e verifica se será necessário troco
     void Dinheiro();
 
 ///@brief Imprime as formas de pagamento para o usuário escolher qual deseja
-///@return o numero correspondente à forma de pagamento escolhida
-    void imprimeFormasPagamento();
+    void ImprimeFormasPagamento();
 
-///@brief Coleta os dados do cartão (nome do titular, número do cartao, validade e cvv) e armazena na struct caso a forma de pagamento seja cartão de crédito ou débito
-///@param cartao Struct que contém os dados do cartão a serem armazenados
-    void coletaDadosCartao (Cartao& cartao);
-	
-    Cartao cartaoCred;      // Dados do cartao de credito
-    Cartao cartaoDeb;       // Dados do cartao de debito
 };
 
 #endif
